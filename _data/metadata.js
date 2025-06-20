639ac20a6d6488215e2d0d8157fca34a1041e870
jameslawler <github@jameslawler.com>
2025-06-20 18:02:02 +0100

    Fix site description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "James Lawler",
   url: "https://www.jameslawler.com/",
   language: "en",
-  description: "I am writing about my experiences as a naval navel-gazer.",
+  description: "I am writing about my experiences as a software developer.",
   author: {
     name: "James Lawler",
     url: "https://www.jameslawler.com/about-me/",
